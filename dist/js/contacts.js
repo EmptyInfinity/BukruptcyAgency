b3eec88f9374e891b868ed0c04833fd6a98faffc
EmptyInfinity <soroka14021998@gmail.com>
2019-01-19 22:07:13 +0200

    v.1.2

--- dist/js/contacts.js
+++ dist/js/contacts.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 }, false);
 
-var mymap = L.map('mapid').setView([50.42838275619874, 30.593079113043505], 14);
+var mymap = L.map('mapid').setView([50.4442, 30.49755], 16);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaWx1YSIsImEiOiJjam51MG8wbmMwMTRlM3F1cTJlODd4cjV2In0.7NYLuOal-7aswChQHP1E_w', {
   maxZoom: 18,
@@ -21,7 +21,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
   id: 'mapbox.streets'
 }).addTo(mymap);
 
-L.marker([50.42838275619874, 30.593079113043505]).addTo(mymap)
-  .bindPopup("<h3 class='map-h3' >Banckruptcy agency</h3><p class = 'map-p' >Киев, пр. Павла Тычины, 1в</p>").openPopup();
+L.marker([50.4442, 30.497551]).addTo(mymap)
+  .bindPopup("<h3 class='map-h3' >Banckruptcy agency</h3><p class = 'map-p' >Киев, ул. Симона Петлюри , 16/108</p>").openPopup();
 
 var popup = L.popup();
